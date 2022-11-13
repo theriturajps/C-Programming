83f140732021867f02f4fe081d08944e4640131f
RituRajPS <riturajpsofficial@gmail.com>
2022-11-13 21:43:17 +0530

    Add files via upload

---
+++ arthematic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+// Created by riturajps.in | @RituRajPS
+int main()
+{
+    int square;
+    printf("Enter the Number :");
+    scanf("%d", &square);
+    int sqr = pow(square, 2);
+    printf("The square of the given number is %d", sqr);
+    return 0;
+    // @RituRajPS
+}
