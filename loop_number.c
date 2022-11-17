48954e616fad3b853132db2aa799108bdf28c495
RituRajPS <riturajpsofficial@gmail.com>
2022-11-17 21:32:28 +0530

    Create loop_number.c

---
+++ loop_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// Created by riturajps.in | @RituRajPS
+int main()
+{
+    int a = 0;
+    
+    while (a<=50)
+    {
+        printf("%d\n", a); 
+        a++;
+    }
+    
+    return 0;
+    // @RituRajPS
+}
