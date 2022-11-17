d29a490357e3b733706ff6d7c4bce178346d7978
RituRajPS <riturajpsofficial@gmail.com>
2022-11-17 21:22:24 +0530

    Create loop_number_print.c

---
+++ loop_number_print.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+// Created by riturajps.in | @RituRajPS
+int main()
+{
+    int a = 1;
+
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+
+    return 0;
+    // @RituRajPS
+}
