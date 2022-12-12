2b03d743f85de2a69d31786937d6e82b99d75bc9
RituRajPS <theriturajps@gmail.com>
2022-12-12 20:18:45 +0530

    Create while_loop.c

---
+++ while_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+// Created by github.com/theriturajps | @RituRajPS <----
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    while(a<10){
+        printf("%d\n", a);
+        a++;
+    }
+    return 0;
+    // @RituRajPS <----
+}
