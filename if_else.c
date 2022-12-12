86d0a2f78410aa24906557e9a3e20a6b046a68f1
RituRajPS <theriturajps@gmail.com>
2022-12-12 20:08:50 +0530

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// Created by github.com/theriturajps | @RituRajPS <----
+int main()
+{
+    int age;
+    printf("Enter your age : ");
+    scanf("%d", &age);
+    if(age>=18){
+        printf("You are ready to vote");
+    } else {
+        printf("You are not ready to vote");
+    }
+    return 0;
+    // @RituRajPS <----
+}
